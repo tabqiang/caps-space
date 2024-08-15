0c7d441cad20f34ca9147e3c5ca72c3a7ee3f3ce
Caps <838269741@qq.com>
2024-08-15 08:12:59 +0800

    test: ✅ import vitest

---
+++ packages/components/vitest.config.js
@@ -0,0 +1,11 @@
+import { mergeConfig, defineConfig } from 'vite';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            environment: 'jsdom',
+        },
+    }),
+);
